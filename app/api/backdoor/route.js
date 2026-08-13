0b4b71f0fd64b849184e113b366ba3f7172a6658
0ko0 <danganhvu000@gmail.com>
2026-08-13 13:41:56 +0700

    Update route.js

--- app/api/backdoor/route.js
+++ app/api/backdoor/route.js
@@ -1,6 +1,7 @@
 import { supabase } from '@/lib/supabase';
 import { NextResponse } from 'next/server';
 
+export const runtime = 'edge';
 export const dynamic = 'force-dynamic';
 export const revalidate = 0;
 
